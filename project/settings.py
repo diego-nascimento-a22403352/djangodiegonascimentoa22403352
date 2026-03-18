d2b835b3d94a2e655387a22b187dc2eff9f7c697
Diego Nascimento <a22403352@alunos.ulht.pt>
2026-03-18 14:02:52 +0000

    Projeto Atualizado

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "pessoas",
+    "escolas",
+    "ginasios",
+    "receitas",
+    "lojas",
+    "quizzs",
 ]
 
 MIDDLEWARE = [
---
+++ quizzs/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class QuizzsConfig(AppConfig):
+    name = "quizzs"
